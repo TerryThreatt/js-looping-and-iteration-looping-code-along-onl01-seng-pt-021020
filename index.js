a62f88b94c96ff3a6862386dd0a92b19c8682614
TerryThreatt <terry.threatt@gmail.com>
2020-08-06 14:46:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let thankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouCards;
+}
+
+function countDown(num) {
+    for (let countup = 0; countup < 11; countup++) {
+        console.log(countup);
+      }
+}
